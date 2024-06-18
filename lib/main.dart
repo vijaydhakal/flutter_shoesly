dc0c1892d1d2b6b1214d1da5bec435c1e6eb9b2f
vijaydhakal <vijaydhakal48@gmail.com>
2024-06-18 11:18:23 +0545

    project added

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,7 @@ void main() async {
   runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp(
-        options: FirebaseOptions(
-      apiKey: 'AIzaSyD5S7F5O0TyK2ylTqYmpHQci-Bjvfk9DYk',
-      appId: '1:22188474003:android:39a914f4baf4048542d766',
-      messagingSenderId: '22188474003',
-      projectId: 'shoesly-a39a3',
-      storageBucket: "shoesly-a39a3.appspot.com",
-    ));
+      );
 
     runApp(
       App(env: EnvValue.development),
